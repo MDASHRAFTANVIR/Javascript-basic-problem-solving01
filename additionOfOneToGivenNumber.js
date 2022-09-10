0c3230fdfee02ee4a04a0219dbb5c54c2f9d3185
MDASHRAFTANVIR <ashraftanvir143@gmail.com>
2022-09-11 00:39:00 +0600

    Enter a number and get the sum from 1 to entered number

--- additionOfOneToGivenNumber.js
+++ additionOfOneToGivenNumber.js
@@ -2,9 +2,10 @@
 
 function givenNumber(enterValue){
     sum = 0;
-    for(i=0; i<enterValue.length; i++){
-        sum = sum + i;
+    for(i=0; i<= enterValue; i++){
+        sum = sum + enterValue;
     }return sum;
 }
-let enterUserValue = givenNumber(90);
-console.log(enterUserValue);
+let enterUserValue = givenNumber(parseFloat(prompt("Please Enter a Number to Get the Addition of 1 to Your Input Number:")));
+
+document.write("<h2> Total sum from 1 to your given value is: "  + enterUserValue + "</h2>");
