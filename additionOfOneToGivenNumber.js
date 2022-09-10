9cfb8e789e9b67e8a05be234b89340f979964d72
MDASHRAFTANVIR <ashraftanvir143@gmail.com>
2022-09-11 01:27:07 +0600

    Sum of 1 to n Number

--- additionOfOneToGivenNumber.js
+++ additionOfOneToGivenNumber.js
@@ -8,4 +8,5 @@ function givenNumber(enterValue){
 }
 let enterUserValue = givenNumber(parseFloat(prompt("Please Enter a Number to Get the Addition of 1 to Your Input Number:")));
 
-document.write("<h2> Total sum from 1 to your given value is: "  + enterUserValue + "</h2>");
+
+document.write("<h2> Sum of 1 to ", i-1 ," is : ",  enterUserValue , "</h2>");
